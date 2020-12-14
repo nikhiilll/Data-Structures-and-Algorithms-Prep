087694b97b20f0fbd761737655ce0f738c7e647c
nikhiilll <pawarnikhilp@gmail.com>
2020-12-14 23:57:18 +0530

    EOD 12/14/20

---
+++ Arrays/LeetCode/TwoSum_1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+        numdict = {}
+        
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in numdict:
+                return ([numdict[diff], i])
+            numdict[nums[i]] = i
